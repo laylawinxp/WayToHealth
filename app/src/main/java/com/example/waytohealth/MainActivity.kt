6aff8328d8b8305907f7bde3069b63499672cd35
Viktoria Dombrovskaya <spamovik123@yandex.ru>
2024-09-19 22:41:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/waytohealth/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.waytohealth
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.waytohealth.fragments.HomeFragment
+import com.example.waytohealth.fragments.ProfileFragment
+import com.example.waytohealth.fragments.TrainingFragment
+import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val bottomNavigation = findViewById<CurvedBottomNavigation>(R.id.bottomNavigation)
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(1, "Главная", R.drawable.home)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(2, "Тренировки", R.drawable.dumbbell)
+        )
+        bottomNavigation.add(
+            CurvedBottomNavigation.Model(3, "Профиль", R.drawable.user)
+        )
+
+        bottomNavigation.setOnClickMenuListener {
+            when(it.id){
+                1 -> {
+                    replaceFragment(HomeFragment())
+                }
+                2 -> {
+                    replaceFragment(TrainingFragment())
+                }
+                3 -> {
+                    replaceFragment(ProfileFragment())
+                }
+            }
+        }
+        replaceFragment(TrainingFragment())
+        bottomNavigation.show(2)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
