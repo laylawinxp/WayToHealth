05d6044356bfca28a1b738a5fb505259500d46b9
Viktoria Dombrovskaya <spamovik123@yandex.ru>
2024-09-20 00:10:47 +0300

    добавление гантельки

--- app/src/main/java/com/example/waytohealth/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/waytohealth/fragments/HomeFragment.kt
@@ -6,32 +6,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CalendarView
+import com.applandeo.materialcalendarview.CalendarView
+import androidx.core.content.ContextCompat
 import com.applandeo.materialcalendarview.CalendarDay
+import com.applandeo.materialcalendarview.EventDay
 import com.example.waytohealth.R
 import java.util.Calendar
 
 class HomeFragment : Fragment() {
 
+    private lateinit var calendarView: CalendarView
+    private var events: MutableMap<String,String> = mutableMapOf()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val calendarDays: MutableList<CalendarDay> = ArrayList<CalendarDay>()
-
-        val calendar = Calendar.getInstance()
-        val calendarDay = CalendarDay(calendar)
-        calendarDay.imageResource = R.drawable.dumbbell
-        calendarDays.add(calendarDay)
-
-
-//or if you want to specify event label color
-        //calendarDay.labelColor = Color.parseColor("#228B22")
-        //calendarDays.add(calendarDay)
-
-            // val calendarView: CalendarView = requireView().findViewById(R.id.calendarView)
-
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        calendarView = requireView().findViewById(R.id.calendarView)
+
+        val calendars: ArrayList<CalendarDay> = ArrayList()
+        val calendar = Calendar.getInstance()
+        calendar.set(2024, 7,20)
+        val calendarDay = CalendarDay(calendar)
+        calendarDay.labelColor = R.color.pink
+        calendarDay.imageResource = R.drawable.dumbbell
+        calendars.add(calendarDay)
+        events["10-09-2024"] = "Training"
+        calendarView.setCalendarDays(calendars)
     }
 
 
